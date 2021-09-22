ad0a8cc2e0917d2316252b4bb20cf5fdccd3a91e
GiovanniTseng <90911193+GiovanniTseng@users.noreply.github.com>
2021-09-22 21:52:58 +0800

    Add files via upload
    first assigment

---
+++ 20210917.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 17 20:15:36 2021
+
+@author: giotseng
+""";
+name=input('你叫什麼名字?')
+print('你叫'+name)
+
+food=input('你吃了什麼?')
+print('你吃'+food)
